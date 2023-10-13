55bdb036bc0dde198c1238e24aa51236b6a5e282
ciflire <leovesse@gmail.com>
2023-10-13 10:26:57 +0200

    tried using nixviù

--- flake.nix
+++ flake.nix
@@ -8,7 +8,13 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  inputs.nixvim = {
+    url = "github:nix-community/nixvim";
+    # If you are not running an unstable channel of nixpkgs, select the corresponding branch of nixvim.
+    # url = "github:nix-community/nixvim/nixos-23.05";
 
+    inputs.nixpkgs.follows = "nixpkgs";
+  };
   outputs = { nixpkgs, home-manager, ... }:
     let
       system = "x86_64-linux";
