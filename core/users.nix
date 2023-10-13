481be712aea181b29b62f1269488ed60688e66bc
ciflire <leovesse@gmail.com>
2023-10-13 10:46:33 +0200

    define zsh as default shell

--- core/users.nix
+++ core/users.nix
@@ -2,6 +2,7 @@
 {
   # TODO: do it with home manager
   users.users.ciflire = {
+    shell = pkgs.zsh;
     isNormalUser = true;
     description = "ciflire";
     extraGroups = [ "networkmanager" "wheel" "postgres"];
