c44868e4134cbba45bf57e81ecfe6cbe0a012b22
ciflire <leovesse@gmail.com>
2023-10-15 19:39:57 +0200

    font config done

--- core/fonts.nix
+++ core/fonts.nix
@@ -1,6 +1,16 @@
 { config, pkgs, ... }:
 {
-  fonts.packages = [
-    pkgs.nerdfonts
-  ];
+  fonts = {
+    packages = [
+      pkgs.nerdfonts
+    ];
+    fontDir.enable = true;
+    fontconfig = {
+      defaultFonts = {
+        monospace = [ "FiraCode Nerd Font Mono" ];
+      };
+    };
+  };
+
+  # console.font = "FiraCode Nerd Font Mono";
 }
