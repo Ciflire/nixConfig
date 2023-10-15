0c986eeff4a6160315cd40060357b0096e7fcb3e
ciflire <leovesse@gmail.com>
2023-10-15 16:58:28 +0200

    edited local vars

--- core/localVars.nix
+++ core/localVars.nix
@@ -1,24 +1,23 @@
 { config, pkgs, ... }: {
   # Set your time zone.
   time.timeZone = "Europe/Paris";
-  
+
   # Select internationalisation properties.
-  i18n.defaultLocale = "fr_FR.UTF-8";
+  i18n.defaultLocale = "en_GB.UTF-8";
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "fr_FR.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "fr_FR.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
+  };
+    
   console.keyMap = "fr";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "fr_FR.UTF-8";
-  #   LC_IDENTIFICATION = "fr_FR.UTF-8";
-  #   LC_MEASUREMENT = "fr_FR.UTF-8";
-  #   LC_MONETARY = "fr_FR.UTF-8";
-  #   LC_NAME = "fr_FR.UTF-8";
-  #   LC_NUMERIC = "fr_FR.UTF-8";
-  #   LC_PAPER = "fr_FR.UTF-8";
-  #   LC_TELEPHONE = "fr_FR.UTF-8";
-  #   LC_TIME = "fr_FR.UTF-8";
-  # };
-
   networking.hostName = "g713-nixos"; # Define your hostname.
 
 }
