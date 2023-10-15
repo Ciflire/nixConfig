ca339aefc54bb8474e16ca77da7173765437010d
ciflire <leovesse@gmail.com>
2023-10-15 17:00:14 +0200

    zsh config

--- core/users.nix
+++ core/users.nix
@@ -1,19 +1,25 @@
 { config, pkgs, ... }:
 {
   # TODO: do it with home manager
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-#      vpn = "sudo openconnect -u vesse1u@etu --authgroup='Universite-de-Lorraine' vpn.univ-lorraine.fr";
-      rnm = "sudo systemctl restart NetworkManager";
-      rebuild = "sudo nixos-rebuild switch --flake /home/ciflire/secondFlake#g713-nixos";
-    };
-  };
   users.users.ciflire = {
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "ciflire";
     extraGroups = [ "networkmanager" "wheel" "postgres" ];
   };
-
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    syntaxHighlighting.enable = true;
+    autosuggestions.enable = true;
+    shellAliases = {
+      vpn = "sudo openconnect -u vesse1u@etu --authgroup='Universite-de-Lorraine' vpn.univ-lorraine.fr";
+      rnm = "sudo systemctl restart NetworkManager";
+      rebuild = "sudo nixos-rebuild switch --flake /home/ciflire/secondFlake#g713-nixos";
+    };
+  };
+  programs.starship = {
+    enable = true;
+    settings = ./starship.toml;
+  };
 }
