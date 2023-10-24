5309f422908781a31d3d9932c8d5310cecf2c10b
ciflire <leovesse@gmail.com>
2023-10-24 14:37:29 +0200

    python dev config modified

--- development/python.nix
+++ development/python.nix
@@ -2,8 +2,10 @@
 {
   environment.systemPackages = with pkgs; [
     python3 #compiler 
-    python310Packages.python-lsp-server #lsp
+    # python310Packages.python-lsp-server #lsp
+    nodePackages_latest.pyright #lsp
     ruff-lsp #linter
+    ruff
     python310Packages.autopep8 #formatter
   ];
 }
