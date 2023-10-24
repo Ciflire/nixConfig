3e4ca620d973993e02de9edcef699c745a377a99
ciflire <leovesse@gmail.com>
2023-10-24 12:46:48 +0200

    added hyprland packages

--- hyprland/packages.nix
+++ hyprland/packages.nix
@@ -3,20 +3,37 @@
   # TODO: extra packages of hyprland
   environment.systemPackages = with pkgs; [
     alacritty
+    kitty
     arandr
     brightnessctl
     cinnamon.nemo-with-extensions
+    clapper
     dunst # notification system
     eww-wayland # status bar
     firefox-wayland
     gitkraken
     grim
+    webcord
     jq
     libreoffice-qt
+    lutris
+    nwg-displays
+    nwg-bar
+    nwg-menu
+    nwg-dock
+    nwg-panel
+    nwg-drawer
+    nwg-wrapper
+    nwg-launchers
+    nwg-look  
+    obs-studio
+    wlr-randr
     playerctl
     polkit # authentication agent
     slurp
     spotify
+    steam
+    steam-run
     swappy
     sway
     swaylock-effects # Screenlock
@@ -25,11 +42,12 @@
 
     xdg-desktop-portal-hyprland
     xdg-utils
-
+    xorg.xeyes
     xfce.thunar
     xournalpp
     waybar # status bar
     wireplumber # sound
+    wdisplays
     wl-clipboard # clipboard
     wlogout # logout manager
     wofi # app launchers
