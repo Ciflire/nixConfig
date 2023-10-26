17e796090db2a96644df25fd2f131338841066e6
ciflire <leovesse@gmail.com>
2023-10-26 08:49:58 +0200

    typescript formatter added

--- development/lsp.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-  environment.systemPackages = with pkgs; [
-    typst-lsp
-    nodePackages_latest.bash-language-server
-    rnix-lsp
-    gopls
-  ];
-}
--- development/typescript.nix
+++ development/typescript.nix
@@ -3,5 +3,6 @@
   environment.systemPackages = with pkgs;[
     nodePackages_latest.typescript
     nodePackages_latest.typescript-language-server
+    nodePackages_latest.prettier
   ];
 }
