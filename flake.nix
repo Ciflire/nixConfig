ce4eb303d51351f4f2f638e3325fb7b6ca7b687c
ciflire <leovesse@gmail.com>
2023-10-26 08:57:18 +0200

    home-manager 23.05 -> master

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = github:nix-community/home-manager;
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs-master";
     };
     helix.url = "github:helix-editor/helix";
