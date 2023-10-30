7addbe66dd67c3c3c6026b538dade45b4ea405ab
VESSE Léo <leovesse@gmail.com>
2023-10-30 13:30:30 +0100

    rust dev config added

--- development/rust.nix
+++ development/rust.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs; [
-
+    rustc # compiler
+    rustup # rust updater
+    cargo # package manager
+    rust-analyzer-unwrapped #lsp
+    rustfmt # formatter
   ];
 }
