3854463f73befc519e8e5bcc8d0b142460279e89
VESSE Léo <leovesse@gmail.com>
2023-11-13 11:50:01 +0100

    added ciflire to wireshark group

--- core/users.nix
+++ core/users.nix
@@ -5,7 +5,7 @@
     shell = pkgs.zsh;
     isNormalUser = true;
     description = "ciflire";
-    extraGroups = [ "networkmanager" "wheel" "postgres" "docker" ];
+    extraGroups = [ "networkmanager" "wheel" "postgres" "docker" "wireshark"];
   };
 
   programs.zsh = {
