5da048b17a5c74c711316e5059fd191a3a97f3bf
VESSE Léo <leovesse@gmail.com>
2023-11-30 13:51:41 +0100

    openconnect package added

--- core/network.nix
+++ core/network.nix
@@ -4,5 +4,9 @@
   networking.networkmanager.enable = true;
   networking.networkmanager.wifi.backend = "wpa_supplicant";
   networking.wireless.userControlled.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    openconnect
+  ];
 }
 
