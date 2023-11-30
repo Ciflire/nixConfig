636ec118634d5d2b9f9b1fe9e6898ac8b1ac4cb8
VESSE Léo <leovesse@gmail.com>
2023-11-30 13:52:05 +0100

    mongodb compass added

--- development/sql.nix
+++ development/sql.nix
@@ -1,8 +1,9 @@
-{config, lib, pkgs, ...}:
+{ config, lib, pkgs, ... }:
 {
   environment.systemPackages = with pkgs;[
     pgadmin4
     mariadb
+    mongodb-compass
     mysql-workbench
     sqlite
     sqls
